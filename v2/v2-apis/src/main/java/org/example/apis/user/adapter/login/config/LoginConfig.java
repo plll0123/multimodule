356889d0040c49d51d4187394dae5cd1b670a128
plll0123 <jsh951227@gmail.com>
2023-09-11 19:17:08 +0900

    ã컴파일 에러 제거

--- v2/v2-apis/src/main/java/org/example/apis/user/adapter/login/config/LoginConfig.java
+++ v2/v2-apis/src/main/java/org/example/apis/user/adapter/login/config/LoginConfig.java
@@ -1,10 +1,14 @@
 package org.example.apis.user.adapter.login.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class LoginConfig implements WebMvcConfigurer {
 
-
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        WebMvcConfigurer.super.addInterceptors(registry);
+    }
 }
--- v2/v2-core/article/src/main/java/org/example/domain/article/Title.java
+++ v2/v2-core/article/src/main/java/org/example/domain/article/Title.java
@@ -2,14 +2,14 @@ package org.example.domain.article;
 
 import java.util.regex.Pattern;
 
-public class Title extends DomainValueObject {
+public class Title  {
 
     private final String title;
     private static final Pattern TITLE_PATTERN =
             Pattern.compile("^[가-힣]{2,5}$");
 
     public Title(final String title) {
-        if (doesntMatch(TITLE_PATTERN, title))
+        if (TITLE_PATTERN.matcher(title).matches())
             throw new IllegalArgumentException();
         this.title = title;
     }
